607b5e666b16012df8cf6f5be54d33d6ed9e2b40
Lebogang <lebogangodeveloper@gmail.com>
2023-07-23 15:11:31 +0200

    Contain functions prototypes

--- sort.h
+++ sort.h
@@ -40,7 +40,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-/*void quick_sort_recursive(int *array, ssize_t low, ssize_t high,
-  size_t size);*/
 
 #endif
