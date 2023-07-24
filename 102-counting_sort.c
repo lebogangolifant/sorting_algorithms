e0874e07e2aa27b0d69d090c4febdac7ded8c490
Lebogang <lebogangodeveloper@gmail.com>
2023-07-24 15:22:47 +0200

    Sorts an array of integers in ascending order using the Counting sort algorithm

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -9,40 +9,44 @@
 
 void counting_sort(int *array, size_t size)
 {
-	int value;
 	size_t index;
-	int sorted_index = 0;
-	size_t max_value = array[0];
-	int *counting_array = malloc((max_value + 1) * sizeof(int));
+	int max_value = array[0];
+	int *counting_array, *sorted_array;
 
-	if (size <= 1)
+	if (!array || size < 2)
 		return;
 
 	for (index = 1; index < size; index++)
 	{
-		if ((size_t)array[index] > max_value)
+		if (array[index] > max_value)
 			max_value = array[index];
 	}
-
+	counting_array = malloc((max_value + 1) * sizeof(int));
 	if (!counting_array)
 		return;
 
-	for (index = 0; index <= max_value; index++)
-		counting_array[index] = 0;
-
-	for (index = 0; index < size; index++)
-		counting_array[array[index]]++;
-
-	print_array(counting_array, max_value + 1);
-
-	for (value = 0; value <= (int)max_value; value++)
+	sorted_array = malloc(size * sizeof(int));
+	if (!sorted_array)
 	{
-		while (counting_array[value] > 0)
-		{
-			array[sorted_index++] = value;
-			counting_array[value]--;
-		}
+		free(counting_array);
+		return;
 	}
 
+	for (index = 0; index <= (size_t)max_value; index++)
+		counting_array[index] = 0;
+	for (index = 0; index < size; index++)
+		counting_array[array[index]]++;
+	for (index = 1; index <= (size_t)max_value; index++)
+		counting_array[index] += counting_array[index - 1];
+	print_array(counting_array, max_value + 1);
+
+	for (index = 0; index < size; index++)
+	{
+		sorted_array[counting_array[array[index]] - 1] = array[index];
+		counting_array[array[index]]--;
+	}
+	for (index = 0; index < size; index++)
+		array[index] = sorted_array[index];
 	free(counting_array);
+	free(sorted_array);
 }
